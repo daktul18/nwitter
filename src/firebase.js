d7c2a566de3b97e43af416cef9ed2f488265dd39
Hoon <daktul18@gmail.com>
2022-08-08 21:48:45 +0900

    second step

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-AsxVFjNJH9jmGapI3O0fY6tTB0UwLcI",
-  authDomain: "nwitter-8f465.firebaseapp.com",
-  projectId: "nwitter-8f465",
-  storageBucket: "nwitter-8f465.appspot.com",
-  messagingSenderId: "305753686532",
-  appId: "1:305753686532:web:dd053cfa39d6e3ab912b51"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
